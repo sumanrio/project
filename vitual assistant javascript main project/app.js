465e88f6b6520971deada5abb378163cbaea23b0
sumanrio <119526968+sumanrio@users.noreply.github.com>
2023-03-13 23:16:54 +0530

    Add files via upload

---
+++ vitual assistant javascript main project/app.js
@@ -0,0 +1,138 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+function speak(sentence) {
+    const text_speak = new SpeechSynthesisUtterance(sentence);
+
+    text_speak.rate = 1;
+    text_speak.pitch = 1;
+
+    window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe() {
+    var day = new Date();
+    var hr = day.getHours();
+
+    if(hr >= 0 && hr < 12) {
+        speak("Good Morning Boss");
+    }
+
+    else if(hr == 12) {
+        speak("Good noon Boss");
+    }
+
+    else if(hr > 12 && hr <= 17) {
+        speak("Good Afternoon Boss");
+    }
+
+    else {
+        speak("Good Evening Boss");
+    }
+}
+
+window.addEventListener('load', ()=>{
+    speak("Activating g-one");
+    speak("Going online");
+    wishMe();
+})
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+let windowsB = []  
+
+recognition.onresult = (event) => {
+    const current = event.resultIndex;
+    const transcript = event.results[current][0].transcript;
+    content.textContent = transcript;
+    speakThis(transcript.toLowerCase());
+}
+
+btn.addEventListener('click', ()=>{
+    recognition.start();
+})
+
+function speakThis(message) {
+    const speech = new SpeechSynthesisUtterance();
+
+    speech.text = "I did not understand what you said please try again";
+
+    if(message.includes('hey') || message.includes('hello')) {
+        const finalText = "Hello Boss";
+        speech.text = finalText;
+    }
+
+    if(message.includes('how are you')) {
+        const finalText = "I am fine boss tell me how can i help you";
+        speech.text = finalText;
+    }
+
+    if(message.includes('name')) {
+        const finalText = "My name is G-one";
+        speech.text = finalText;
+    }
+
+    if(message.includes('close all tabs')) {
+        const finalText = "closing all tabs";
+        speech.text = finalText;
+        windowsB.forEach((e) =>{
+            e.close()
+        })
+    }
+
+    if(message.includes('open google')) {
+        let a = window.open("https://www.google.com");
+        readOut("Opening Google");
+        windowsB.push(a)
+    }
+
+    if(message.includes('open instagram')) {
+        let a = window.open("https://instagram.com");
+        const finalText = "Opening instagram";
+        speech.text = finalText;
+        windowsB.push(a)
+    }
+
+    if(message.includes('what is') || message.includes('who is') || message.includes('what are')) {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "This is what i found on internet regarding " + message;
+        speech.text = finalText;
+    }
+
+    if(message.includes('wikipedia')) {
+        window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "")}`, "_blank");
+        const finalText = "This is what i found on wikipedia regarding " + message;
+        speech.text = finalText;
+    }
+
+    if(message.includes('time')) {
+        const time = new Date().toLocaleString(undefined, {hour: "numeric", minute: "numeric"})
+        const finalText = time;
+        speech.text = finalText;
+    }
+
+    if(message.includes('date')) {
+        const date = new Date().toLocaleString(undefined, {month: "short", day: "numeric"})
+        const finalText = date;
+        speech.text = finalText;
+    }
+
+    if(message.includes('calculator')) {
+        window.open('Calculator:///')
+        const finalText = "Opening Calculator";
+        speech.text = finalText;
+    }
+
+    if(message.includes('search on google')) {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "I found some information for " + message + " on google";
+        speech.text = finalText;
+    }
+
+    speech.volume = 1;
+    speech.pitch = 1;
+    speech.rate = 1;
+
+    window.speechSynthesis.speak(speech);
+}
